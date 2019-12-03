be235956617cc1b0b449c1604dcc3d2bdd0fc0d7
Neryad <dayern.gomez@hotmail.com>
2019-12-02 23:05:41 -0400

    new function to route

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {HeroesService, Heroe} from '../../services/heroes.service';
+import { Router } from '@angular/router'; 
 
 
 @Component({
@@ -11,12 +12,17 @@ export class HeroesComponent implements OnInit {
 
   heroes:Heroe[] = [];
 
-  constructor(private _heroesService:HeroesService) { }
+  constructor(private _heroesService:HeroesService,
+              private _router:Router  ) { }
 
   ngOnInit() {
     this.heroes = this._heroesService.getHeroes();
-    console.log(this.heroes);
+    // console.log(this.heroes);
     
   }
-
+  verHeroe(idx:number){
+    this._router.navigate( ['/heroe',idx] )
+    console.log(idx);
+    
+  }
 }
